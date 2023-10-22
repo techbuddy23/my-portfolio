930c378aa91050e5da65c0fddc19030a301f4477
techbuddy23 <141866503+techbuddy23@users.noreply.github.com>
2023-10-22 23:12:12 +0530

    Update About.js

--- src/About.js
+++ src/About.js
@@ -5,10 +5,10 @@ const About = () => {
     <div className="section">
       <h2>About Me</h2>
       <p>I am highly motivated BTech CSE student of VIT Vellore, with robust academic background </p>
-        <p>nd versatile skill-set in programming and advanced technologies. My hands-on experience in design</p>
-       <p> and development of next generation software in the field of Data Analytics, AI, ML and IoT during </p>
-       <p> internships at C-DAC and Netmax Technologies, reflects my practical expertise. I have a track record </p>
-       <p> of successful projects, excelling in hackathons and exhibiting strong leadership</p>
+        <p>nd versatile skill-set in programming and advanced technologies. I have hands-on experience in design</p>
+       <p> and development of next generation software in the field of Data Analytics, AI, ML and IoT. </p>
+       <p> I have a track record of successful projects, excelling in hackathons and exhibiting strong leadership </p>
+
     </div>
   );
 };
