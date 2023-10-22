5282b3adec3d7d95bbe95515e1854174208361be
techbuddy23 <141866503+techbuddy23@users.noreply.github.com>
2023-10-22 23:07:29 +0530

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="section">
+      <h2>About Me</h2>
+      <p>I am highly motivated BTech CSE student of VIT Vellore, with robust academic background </p>
+        <p>nd versatile skill-set in programming and advanced technologies. My hands-on experience in design</p>
+       <p> and development of next generation software in the field of Data Analytics, AI, ML and IoT during </p>
+       <p> internships at C-DAC and Netmax Technologies, reflects my practical expertise. I have a track record </p>
+       <p> of successful projects, excelling in hackathons and exhibiting strong leadership</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="section">
+      <h2>Contact Me</h2>
+      <p>Email: aviral.jain2021a@vitstudetn.ac.in</p>
+      <p>Phone: 9588217939</p>
+      <p>LinkedIn:https://www.linkedin.com/in/aviral-jain-098992247</p>
+    </div>
+  );
+};
+
+export default Contact;
